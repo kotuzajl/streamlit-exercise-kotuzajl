7031020028b9445fc5d1d3437db196fe674a284b
kotuzajl <71093522+kotuzajl@users.noreply.github.com>
2025-10-28 14:09:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.title("Medals Visualization")
 medal = st.selectbox("Medal type", ["gold",'silver','bronze'])
 
 #Checkboxes
-show_bar = st.checkboxes("Show Bar Chart", value = True)
-show_pie = st.checkboxes("Show Pie Chart", value = True)
+show_bar = st.checkbox("Show Bar Chart", value = True)
+show_pie = st.checkbox("Show Pie Chart", value = True)
 
 #two-col structure
 col1,col2 = st.columns(2)
